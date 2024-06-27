169215f80847fb958f7b28bc52acaf6578f98f0f
Daniel Llumigusin <dgllumigusin@espe.edu.ec>
2024-06-27 08:37:23 -0500

    Vallidacion
    Validacion de datos iguales

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,25 +11,39 @@ function App() {
   const [reservados,setReservados] = useState([])
 
 
+
   function Guardar(filas_temporales, asientos_temporales){
-    setReservados([...reservados,[filas_temporales, asientos_temporales]]);
-    console.log(reservados)
+    const alreadyReserved = reservados.some(([fila, asiento]) => fila === filas_temporales && asiento === asientos_temporales);
+    if (alreadyReserved) {
+      return alert("Error, el asiento ya ha sido reservado")
+    }else{
+      
+    
+   setReservados([...reservados,[filas_temporales, asientos_temporales]]);
+   console.log(reservados)
+  }
   }
 
   return (
     <div className='container'>
       <div className='form'>
-        <input type='text' placeholder='Ingrese Fila' onChange={(e) => setFilas(e.target.value)}></input>
-        <input type='text' placeholder='Ingrese asiento' onChange={(e) => setAsientos(e.target.value)}></input>
+        <input className="" type='text' placeholder='Ingrese Fila' onChange={(e) => setFilas(e.target.value)}></input>
+        <input className='' type='text' placeholder='Ingrese asiento' onChange={(e) => setAsientos(e.target.value)}></input>
         <button type='button' onClick={(e)=>Guardar(filas, asientos)}>Guardar</button>
       </div>
       <div className='report'>
           <h1>Reporte de Asientos</h1>
           <table>
             <tr>
-              <th>Asientos</th>
               <th>Filas</th>
+              <th>Asientos</th>
             </tr>
+            {reservados.map((reservados, i) =>
+              <tr>
+                <td>{reservados[0]}</td>
+                <td>{reservados[1]}</td>
+              </tr>
+            )}
           </table>
       </div>
     </div>
