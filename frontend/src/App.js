cc404e506dead4d7664ed57a27f5ed526ab1d951
Daniel Llumigusin <dgllumigusin@espe.edu.ec>
2024-06-27 08:53:27 -0500

    Styles 0.1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,50 +1,62 @@
-
-import './App.css';
-import React,{useState} from 'react';
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [filas, setFilas] = useState("");
+  const [asientos, setAsientos] = useState("");
 
+  const [reservados, setReservados] = useState([]);
 
-  const [filas,setFilas] = useState("")
-  const [asientos,setAsientos] = useState("")
-
-  const [reservados,setReservados] = useState([])
-
-
-
-  function Guardar(filas_temporales, asientos_temporales){
-    const alreadyReserved = reservados.some(([fila, asiento]) => fila === filas_temporales && asiento === asientos_temporales);
+  function Guardar(filas_temporales, asientos_temporales) {
+    const alreadyReserved = reservados.some(
+      ([fila, asiento]) =>
+        fila === filas_temporales && asiento === asientos_temporales
+    );
     if (alreadyReserved) {
-      return alert("Error, el asiento ya ha sido reservado")
-    }else{
-      
-    
-   setReservados([...reservados,[filas_temporales, asientos_temporales]]);
-   console.log(reservados)
-  }
+      return alert("Error, el asiento ya ha sido reservado");
+    } else {
+      setReservados([...reservados, [filas_temporales, asientos_temporales]]);
+      console.log(reservados);
+    }
   }
 
   return (
-    <div className='container'>
-      <div className='form'>
-        <input className="" type='text' placeholder='Ingrese Fila' onChange={(e) => setFilas(e.target.value)}></input>
-        <input className='' type='text' placeholder='Ingrese asiento' onChange={(e) => setAsientos(e.target.value)}></input>
-        <button type='button' onClick={(e)=>Guardar(filas, asientos)}>Guardar</button>
+    <div className="container">
+      <div>
+        <div className="form">
+          <input
+            className=""
+            type="text"
+            placeholder="Ingrese Fila"
+            onChange={(e) => setFilas(e.target.value)}
+          ></input>
+          <input
+            className=""
+            type="text"
+            placeholder="Ingrese asiento"
+            onChange={(e) => setAsientos(e.target.value)}
+          ></input>
+          <button type="button" className="btn-guardar" onClick={(e) => Guardar(filas, asientos)}>
+            Guardar
+          </button>
+        </div>
       </div>
-      <div className='report'>
+      <div>
+        <div className="report">
           <h1>Reporte de Asientos</h1>
           <table>
             <tr>
               <th>Filas</th>
               <th>Asientos</th>
             </tr>
-            {reservados.map((reservados, i) =>
+            {reservados.map((reservados, i) => (
               <tr>
                 <td>{reservados[0]}</td>
                 <td>{reservados[1]}</td>
               </tr>
-            )}
+            ))}
           </table>
+        </div>
       </div>
     </div>
   );
