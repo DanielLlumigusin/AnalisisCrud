761861eeb0765e715440ea5d965ca33268113b46
Daniel Llumigusin <dgllumigusin@espe.edu.ec>
2024-06-27 08:07:07 -0500

    Formulario
    Sin estilizar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import React,{useState} from 'react';
 
 function App() {
+
+
+  const [filas,setFilas] = useState("")
+  const [asientos,setAsientos] = useState("")
+
+  const [reservados,setReservados] = useState([])
+
+  function Imprimir(){
+    console.log(filas)
+    console.log(asientos)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div className='form'>
+        <input type='text' placeholder='Ingrese Fila' onChange={(e) => setFilas(e.target.value)}></input>
+        <input type='text' placeholder='Ingrese asiento' onChange={(e) => setAsientos(e.target.value)}></input>
+        <button type='button' onClick={(e)=>Imprimir()}>Guardar</button>
+      </div>
+      <div className='report'>
+          
+      </div>
     </div>
   );
 }
